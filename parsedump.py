d43b9ad6f88ed21d74992adea159282e968a4028
christopher-mills <github@chrisam.net>
2016-01-21 12:53:32 -0800

    Changed output method to csv writer. Added option to write to files

--- parsedump.py
+++ parsedump.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python
-import argparse
+import argparse, csv, sys
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', metavar='FILE', type=str,help='Specify a file name')
 parser.add_argument('-t','--type',type=str,help='Specify the dump type (pony,keybase)')
 parser.add_argument('-d','--delimiter',type=str,help='Specify a delimiter (default is comma)')
-parser.add_argument('-q','--qualifier',type=str,help='Specify a text qualifier to surround the fields (default is none)')
+parser.add_argument('-q','--qualifier',type=str,help='Specify a text qualifier to surround the fields (default is double quotes)')
+parser.add_argument('-o','--outfile',type=str,help='Specify an output file, otherwise defaults to STDOUT')
 args = parser.parse_args()
 f = open(args.filename, 'r')
+delimiter = ','
+qualifier = '"'
 if args.delimiter:
 	delimiter = args.delimiter
-else:
-	delimiter = ','
 if args.qualifier:
 	qualifier = args.qualifier
-else:
-	qualifier = ''
+if args.outfile:
+	outfile = open(args.outfile,'w')
 for line in f:
 	if args.type == 'pony':
+		# Pony format is: protocol://username:password@site
 		userPass=line.partition('//')[2].rpartition('@')[0]
 		user=userPass.partition(':')[0]
 		password=userPass.partition(':')[2]
-		print qualifier + user + qualifier + delimiter + qualifier + password + qualifier
 	if args.type == 'keybase':
+		# Keybase format is: PCName[space]Site[space]Username[space]Password
 		pcname=line.partition(' ')[0]
-		password=line.rpartition(' ')[2]
+		password=line.rpartition(' ')[2].rstrip('\n')
 		user=line.rpartition(' ')[0].rpartition(' ')[2]
-		print qualifier + user + qualifier + delimiter + qualifier + password + qualifier,
+	# Dunno how to program the output without the if-statement.
+        if args.outfile:
+        	dumpwriter = csv.writer(outfile,delimiter=delimiter,quotechar=qualifier,quoting=csv.QUOTE_MINIMAL)
+        else:
+                dumpwriter = csv.writer(sys.stdout,delimiter=delimiter,quotechar=qualifier,quoting=csv.QUOTE_MINIMAL)
+        dumpwriter.writerow([user,password])
